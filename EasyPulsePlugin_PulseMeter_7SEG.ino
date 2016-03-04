95fac3db9700e4a0ab898e4442dcce1658d4c5ac
hyeh20 <hughyeh1@gmail.com>
2016-03-03 21:28:23 -0600

    Update EasyPulsePlugin_PulseMeter_7SEG.ino

--- EasyPulsePlugin_PulseMeter_7SEG.ino
+++ EasyPulsePlugin_PulseMeter_7SEG.ino
@@ -12,7 +12,7 @@
 LedControl lc=LedControl(7,6,5,1);
 
 #define Sampling_Time 5
-#define Num_Samples 600
+#define Num_Samples 400
 #define Peak_Threshold_Factor 85
 #define Minimum_Range 500
 #define Minimum_Peak_Separation 75  // 75*5=375 ms
